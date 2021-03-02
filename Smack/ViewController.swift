52721309a7d038743d8e89b98b2b80eaaa224283
abd <abd@Ekrems-MacBook-Air.local>
2021-03-02 18:48:29 +0300

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by abd on 2.03.2021.
+//  Copyright © 2021 abd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
