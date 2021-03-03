693c3d46ae58eca32bcd7e1d5f1d30c2d130f640
abd <abd@Ekrems-MacBook-Air.local>
2021-03-03 17:32:33 +0300

    ders 2 sonu

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by abd on 3.03.2021.
+//  Copyright © 2021 abd. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60
+    }
+    
+
+ 
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by abd on 3.03.2021.
+//  Copyright © 2021 abd. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector (SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController()!.panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController()!.tapGestureRecognizer())
+        
+    }
+    
+
+   
+
+}
